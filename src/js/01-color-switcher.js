1bc74732457a986c9e4c82e1fac61898fad40090
annared <annared1982@gmail.com>
2023-11-29 23:10:32 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function setRandomBC() {
+  const bodyRef = document.querySelector('body');
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', 'true');
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopSetRandomBC() {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'true');
+  clearInterval(timerId);
+}
+
+const startBtn = document.querySelector('button[data-start]');
+// console.log('startBtn', startBtn);
+
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.setAttribute('disabled', 'true');
+// console.log('stopBtn', stopBtn);
+
+let timerId = null;
+
+startBtn.addEventListener('click', setRandomBC);
+stopBtn.addEventListener('click', stopSetRandomBC);
