c1c770458e72b50bc27b22d93613faed30f29665
annared <annared1982@gmail.com>
2023-12-06 11:39:42 +0200

    fetch country

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,18 +3,12 @@ const baseUrl = 'https://restcountries.com/v3.1/name/';
 function fetchCountries(countryName) {
   return fetch(
     `${baseUrl}${countryName}/?fields=name,capital,population,flags,languages`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(response => console.log(response));
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
-fetchCountries("fr");
-
-//console.log(fetchCountries("france"))
-
-//baseUrl`${countryName}`
+export { fetchCountries };
