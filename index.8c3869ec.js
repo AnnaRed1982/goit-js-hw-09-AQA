60d94b9465fc44e4599dadb861ab6abad9cd5e07
AnnaRed1982 <105372975+AnnaRed1982@users.noreply.github.com>
2023-12-06 09:40:07 +0000

    Deploying to gh-pages from @ AnnaRed1982/goit-js-hw-09-AQA@c1c770458e72b50bc27b22d93613faed30f29665 🚀

--- index.8c3869ec.js
+++
@@ -1,2 +0,0 @@
-fetch(`https://restcountries.com/v3.1/name/${"fr"}/?fields=name,capital,population,flags,languages`).then((t=>{if(!t.ok)throw new Error(t.status);return t.json()})).then((t=>console.log(t)));
-//# sourceMappingURL=index.8c3869ec.js.map
